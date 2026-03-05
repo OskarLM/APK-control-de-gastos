d1bacfbb34c8ca47d7f25288a12ae342b2ac5db8
OskarLM <32792036+OskarLM@users.noreply.github.com>
2026-03-05 18:40:59 +0100

    Update sw.js
    Signed-off-by: OskarLM <32792036+OskarLM@users.noreply.github.com>

--- sw.js
+++ sw.js
@@ -1,10 +1,36 @@
-const CACHE_NAME = 'gastos-oj-v1';
-const ASSETS = ['./', './index.html', './manifest.json'];
+const CACHE_NAME = "gastos-oj-v1";
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+const urlsToCache = [
+  "./",
+  "./index.html"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
 });
