d928c00a831d05dc5fde374c8a42201f17c9cccd
OskarLM <32792036+OskarLM@users.noreply.github.com>
2026-03-06 18:11:51 +0100

    Update sw.js
    Signed-off-by: OskarLM <32792036+OskarLM@users.noreply.github.com>

--- sw.js
+++ sw.js
@@ -1,36 +1,24 @@
-const CACHE_NAME = "gastos-oj-v1";
-
-const urlsToCache = [
-  "./",
-  "./index.html"
+const CACHE_NAME = 'gastos-oj-v3.18';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
+// Instalación y almacenamiento en caché
+self.addEventListener('install', (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
     })
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
+// Estrategia: Primero Red, si falla, Caché
+self.addEventListener('fetch', (evt) => {
+  evt.respondWith(
+    fetch(evt.request).catch(() => {
+      return caches.match(evt.request);
     })
   );
 });
